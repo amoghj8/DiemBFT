f652696115df585ebc2e95d1168406e9b0bcf694
Amogh Joshi <amoghj8@gmail.com>
2021-10-10 00:52:34 -0400

    Fixed indentation for Ledger.py

--- Ledger/Ledger.py
+++ Ledger/Ledger.py
@@ -1,17 +1,17 @@
 class Ledger():
-	"""Ledger Module"""
-	def __init__(self, arg):
-		super(Ledger, self).__init__()
-		self.arg = arg
-	
-	def speculate(prev_block_id, block_id, txns):
-		pass
+    """Ledger Module"""
+    def __init__(self, arg):
+        super(Ledger, self).__init__()
+        self.arg = arg
+    
+    def speculate(prev_block_id, block_id, txns):
+        pass
 
-	def pending_state(block_id):
-		pass
+    def pending_state(block_id):
+        pass
 
-	def commit(block_id):
-		pass
+    def commit(block_id):
+        pass
 
-	def committed_block(block_id):
-		pass
+    def committed_block(block_id):
+        pass
