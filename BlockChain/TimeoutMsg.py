70d5506dc8b464342f5b20ec1810ce6f9527e5be
venkateshvenugopal <venkateshvngpl@gmail.com>
2021-10-13 21:44:11 -0400

    Adding TimeoutMsg and ProposalMsg modules

---
+++ BlockChain/TimeoutMsg.py
@@ -0,0 +1,7 @@
+import TimeoutInfo
+
+class TimeoutMsg:
+    def __init__(self, tmo_info, last_round_tc, high_commit_qc):
+        self.tmo_info = tmo_info
+        self.last_round_tc = last_round_tc
+        self.high_commit_qc = high_commit_qc
