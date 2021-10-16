9f3b73c23b9f48feaed65abb75fe3aa13fb1b6ae
venkateshvenugopal <venkateshvngpl@gmail.com>
2021-10-15 23:12:37 -0400

    Changes to Replica and Pacemaker

--- BlockChain/Pacemaker.py
+++ BlockChain/Pacemaker.py
@@ -8,28 +8,31 @@ from collections import defaultdict
 
 class Pacemaker:
     
-    def __init__(self):
+    def __init__(self, replica):
         self.current_round = None
         self.last_round_tc = None
         self.pending_timeouts = defaultdict(list)
+        self.replica = replica
 
     # set based on the config
     def get_round_timer(self, r):
-        delta = 5              # TODO - fetch from config
+        delta = 5              # Todo - fetch from config
         return 4 * delta
 
     
+    #In Replica
     # Not required - Implement in Replica
     def start_timer(self, new_round):
-        stop_timer(self.current_round)
+        self.replica.round_done = True  # stop_timer(self.current_round)
         self.current_round = new_round
-        start_local_timer(self.get_round_timer())
+        # start_local_timer(self.get_round_timer())
         
     
+    # In Replica
     # Function to be triggered on Timeout - in Replica
     def local_timeout_round(self):
         timeout_info = Safety.make_timeout(self.current_round, BlockTree.high_qc, self.last_round_tc)
-        # TODO - Send BroadCast message
+        # Todo - Send BroadCast message
 
     
     def process_remote_timeout(self, tmo):
@@ -39,14 +42,15 @@ class Pacemaker:
 
         lst_sender = [tmo_v.sender for tmo_v in self.pending_timeouts[tmo_info.round]]
 
-        if tmo_info.sender not in lst:
+        if tmo_info.sender not in lst_sender:
             self.pending_timeouts[tmo_info.round].append(tmo_info)
             lst_sender.append(tmo_info.sender)
 
         # Todo - read f from config
         f = 3
         if len(lst_sender) == f+1:
-            stop_timer(self.current_round)
+            # stop_timer(self.current_round)
+            self.replica.round_done = True # Todo - Not sure if we shld stop timer
             self.local_timeout_round()
 
         lst_high_qc_round = [tmo_v.high_qc for tmo_v in self.pending_timeouts[tmo_info.round]]
