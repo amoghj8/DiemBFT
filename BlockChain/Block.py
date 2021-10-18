b0f4fb11788e4e12d49998220ecc3bc4203ef0f6
Venkatesh Venugopal <92241188+venkateshvenugopal@users.noreply.github.com>
2021-10-18 05:39:04 -0400

    Update Block.py

--- BlockChain/Block.py
+++ BlockChain/Block.py
@@ -3,12 +3,13 @@ from collections import defaultdict
 from QC import QC
 
 class Block:
-    def __init__(self, author : int, round : int, payload : str, qc : QC, id : str):
+    def __init__(self, author : int, round : int, payload : str, qc : QC, id : str, txn_id ):
         self.author = author
         self.round = round
         self.payload = payload
         self.qc = qc
         self.id = id
+        self.txn_id = txn_id
         self.children = []
         #Parent id is redundant. Get from qc.vote info.parent id
         #Also not needed
